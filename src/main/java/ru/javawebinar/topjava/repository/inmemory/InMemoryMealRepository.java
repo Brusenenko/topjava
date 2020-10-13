7f6bbb13fc2cb6a19550606e338fce9eb92421c9
Brusenenko <brusenenko@yandex.ru>
2020-10-13 23:50:07 +0300

    HW02

--- src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
@@ -1,46 +1,63 @@
 package ru.javawebinar.topjava.repository.inmemory;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
 import ru.javawebinar.topjava.util.MealsUtil;
+import ru.javawebinar.topjava.web.SecurityUtil;
 
 import java.util.Collection;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
+@Repository
 public class InMemoryMealRepository implements MealRepository {
-    private final Map<Integer, Meal> repository = new ConcurrentHashMap<>();
+    private static final Logger log = LoggerFactory.getLogger(InMemoryMealRepository.class);
+    private final Map<Integer, Map<Integer, Meal>> repository = new ConcurrentHashMap<>();
     private final AtomicInteger counter = new AtomicInteger(0);
 
     {
-        MealsUtil.meals.forEach(this::save);
+        MealsUtil.meals.forEach(meal -> save(SecurityUtil.authUserId(), meal));
     }
 
     @Override
-    public Meal save(Meal meal) {
+    public Meal save(int userId, Meal meal) {
+        log.info("save {}", meal);
+        Map<Integer, Meal> userMeals;
+        if (repository.get(userId) == null) {
+            userMeals = new ConcurrentHashMap<>();
+            repository.put(userId, userMeals);
+        } else {
+            userMeals = repository.get(userId);
+        }
         if (meal.isNew()) {
             meal.setId(counter.incrementAndGet());
-            repository.put(meal.getId(), meal);
-            return meal;
+            return userMeals.put(meal.getId(), meal);
         }
-        // handle case: update, but not present in storage
-        return repository.computeIfPresent(meal.getId(), (id, oldMeal) -> meal);
+        return userMeals.computeIfPresent(meal.getId(), (id, oldMeal) -> meal);
     }
 
     @Override
-    public boolean delete(int id) {
-        return repository.remove(id) != null;
+    public boolean delete(int userId, int id) {
+        log.info("delete {}", id);
+        Map<Integer, Meal> meals = repository.get(userId);
+        return meals.remove(id) != null;
     }
 
     @Override
-    public Meal get(int id) {
-        return repository.get(id);
+    public Meal get(int userId, int id) {
+        log.info("get {}", id);
+        Map<Integer, Meal> meals = repository.get(userId);
+        return meals.get(id);
     }
 
     @Override
-    public Collection<Meal> getAll() {
-        return repository.values();
+    public Collection<Meal> getAll(int userId) {
+        return repository.get(userId).values();
+        // TODO реализовать список еды возвращать отсортированный в обратном порядке по датам
     }
 }
 
--- src/main/java/ru/javawebinar/topjava/service/MealService.java
+++ src/main/java/ru/javawebinar/topjava/service/MealService.java
@@ -1,9 +1,45 @@
 package ru.javawebinar.topjava.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
 
+import java.util.Collection;
+
+@Service
 public class MealService {
+    private static final Logger log = LoggerFactory.getLogger(MealService.class);
+    private final MealRepository repository;
 
-    private MealRepository repository;
+    public MealService(MealRepository repository) {
+        this.repository = repository;
+    }
 
+    public Meal create(int userId, Meal meal) {
+        log.info("create {}", meal);
+        return repository.save(userId, meal);
+    }
+
+    public void delete(int userId, int id) {
+        log.info("delete {}", id);
+        repository.delete(userId, id);
+    }
+
+    public Meal get(int userId, int id) {
+        log.info("get {}", id);
+        return repository.get(userId, id);
+    }
+
+    public Collection<Meal> getAll(int userId) {
+        log.info("getAll for user #{}", userId);
+        return repository.getAll(userId);
+    }
+
+    public void update(int userId, Meal meal) {
+        log.info("update {}", meal);
+        repository.save(userId, meal);
+
+    }
 }
